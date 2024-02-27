f232aa804ec84ad3330d09669cbc9d12ce75bfa7
meerjavali <meerjavali98@gmail.com>
2024-02-28 00:12:53 +0530

    created login and sign up pages

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent {
+  isLoading = false;
+
+  submitForm(signUpForm:NgForm){
+    console.log(signUpForm.value);
+
+  }
+
+}
