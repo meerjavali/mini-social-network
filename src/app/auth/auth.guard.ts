6f6e13360089de0b757e02d5517f0d3cc43c77cf
meerjavali <meerjavali98@gmail.com>
2024-03-24 17:54:01 +0530

    added guards restrict access to create and edit page before login

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,24 @@
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from "@angular/router";
+import { Observable } from "rxjs";
+import { AuthService } from "./auth.service";
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class AuthGuard {
+    
+
+    constructor(private authService: AuthService, private router: Router){
+
+    }
+    canActivate(route:ActivatedRouteSnapshot, status:RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean>{
+
+        const isAuth = this.authService.getAuth();
+        if(!isAuth)
+        {   this.router.navigate(['/login'])
+            
+        }
+
+        return isAuth;
+
+    }
+}
